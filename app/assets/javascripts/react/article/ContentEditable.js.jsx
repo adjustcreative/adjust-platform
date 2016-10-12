f91604320ce24f9c50df4848062b7314fff4ee4e
atebit <chris@adjustcreative.com>
2016-10-12 16:34:21 -0500

    cleanup

--- app/assets/javascripts/react/article/ContentEditable.js.jsx
+++ app/assets/javascripts/react/article/ContentEditable.js.jsx
@@ -1,28 +1,30 @@
 var ContentEditable = React.createClass({
-    render: function(){
-        // console.log(this.props)
 
-        classes = this.props.className + " editable";
+  shouldComponentUpdate: function(nextProps){
+    return nextProps.html !== this.innerHTML;
+  },
 
-        return <div 
-            className={classes}
-            onInput={this.emitChange} 
-            onBlur={this.emitChange}
-            contentEditable
-            dangerouslySetInnerHTML={{__html: this.props.html}}></div>;
-    },
-    shouldComponentUpdate: function(nextProps){
-        return nextProps.html !== this.innerHTML;
-    },
-    emitChange: function(){
-        var html = this.innerHTML;
-        if (this.props.onChange && html !== this.lastHtml) {
-            this.props.onChange({
-                target: {
-                    value: html
-                }
-            });
-        }
-        this.lastHtml = html;
+  emitChange: function(){
+    var html = this.innerHTML;
+    if (this.props.onChange && html !== this.lastHtml) {
+      this.props.onChange({
+        target: { value: html }
+      });
     }
+    this.lastHtml = html;
+  },
+
+
+  render: function(){
+    classes = this.props.className + " editable";
+    return(
+      <div 
+        className={classes}
+        onInput={this.emitChange} 
+        onBlur={this.emitChange}
+        contentEditable
+        dangerouslySetInnerHTML={{__html: this.props.html}}>
+      </div>
+    )
+  }
 });
