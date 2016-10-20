684e8667022d2da99fd5bf532dfbff0bc42a546f
atebit <chris@adjustcreative.com>
2016-10-20 11:12:01 -0500

    homepage

---
+++ app/assets/javascripts/components/homepage.js
@@ -0,0 +1,72 @@
+
+
+
+var Homepage = {
+
+  slides: [],
+  currentIndex: 0,
+
+  init: function(){
+    console.log("hi");
+
+    var self = this;
+
+    $(".home-features").css({
+      position: "relative",
+      overflow: "hidden",
+      "min-height": $(window).height()-80
+    });
+
+    $(".home-feature-controls").css({
+      position:"absolute",
+      bottom:"0px",
+      left:$(window).width()/2,
+    });
+
+
+    $(".home-features .feature").each( function(i, target){
+      var slide = $(this);
+      slide.css({
+        // "position": "absolute",
+        width: "100%",
+        "min-height": $(window).height() * 0.8,
+        left: $(window).width() * i
+      });
+      self.slides.push( slide );
+
+
+    });
+    $(window).resize( function(){ self.resize(); });
+  },
+
+
+  resize: function(){
+    $(".home-features").css({
+      position: "relative",
+      "min-height": $(window).height()
+    });
+    for(var i=0; i<this.slides.length; i++){
+      var slide = this.slides[i];
+      slide.css({
+        width: "100%",
+        "min-height": $(window).height() * 0.8,
+      });
+    }
+  },
+
+
+
+
+
+
+
+};
+
+
+
+$(document).ready(function(){
+  if( HOMEPAGE ){
+    var homepage = Object.create( Homepage );
+    homepage.init(); 
+  }
+});
