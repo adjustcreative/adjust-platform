a601650d5ef202da0d3ee011bf4a09fb241fd51f
atebit <chris@adjustcreative.com>
2016-10-20 05:54:07 -0500

    page content match edit page

--- app/assets/javascripts/react/page/Page.jsx
+++ app/assets/javascripts/react/page/Page.jsx
@@ -13,8 +13,26 @@ var Page = React.createClass({
     
     var bodyClasses = "medium-element body";
 
+
+    var headerStyle = {}
+    if( this.props.featured_image ){
+      headerStyle = {
+        backgroundImage: 'url(' + this.props.featured_image + ')',
+        backgroundSize: "cover"
+      }
+    }
+
     return(
       <article>
+
+        <header className="article-header" style={headerStyle}>
+        
+          <div className="article-header-content">
+            <p className="eyebrow">Case Study</p>
+            <h1>{this.props.title}</h1>
+            <h3>{this.props.subtitle}</h3>
+          </div>
+        </header>
 
         <div className={bodyClasses} dangerouslySetInnerHTML={{__html: this.props.body}} />
 
